54952ee3ef6c1f4e2cc9dcf3d1f2be466311aa95
Florin <sava.florin93@yahoo.com>
2026-07-01 11:55:02 +0300

    change product names in main

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from product_manager import ProductManager
 manager = ProductManager()
 
 
-manager.add_product(Product("Laptop", 5, 3500))
-manager.add_product(Product("Mouse", 20, 80))
-manager.add_product(Product("Tastatura", 10, 150))
+manager.add_product(Product("Casa", 5, 3500))
+manager.add_product(Product("Masina", 20, 80))
+manager.add_product(Product("Canapea", 10, 150))
 
-print("Lista de produse:")
-manager.show_products()
-
-total = manager.show_total_value()
-print(f"Valoarea totala a inventarului este: {total}")
